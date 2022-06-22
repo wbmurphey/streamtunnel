51aa5d18af3259649fc9836f2bc3c781f2d7e4d8
bill <wbmurphey@gmail.com>
2022-06-22 13:42:19 -0600

    fixed syntax error

--- st_setup2.sh
+++ st_setup2.sh
@@ -11,7 +11,7 @@ if [ "$os" == "mac" ]; then
     echo "(trap 'kill 0' SIGINT; ssh -vNL 4554:\$2:554 $atlas_username@\$1 & sleep 3 && /Applications/VLC.app/Contents/MacOS/VLC rtsp://localhost:4554)" >> $HOME/scripts/zsh/streamtunnel.sh
     chmod +x $HOME/scripts/zsh/streamtunnel.sh
     sudo ln -s $HOME/scripts/zsh/streamtunnel.sh /usr/local/bin/streamtunnel
-elif [ "$os" == "ubuntu"]; then
+elif [ "$os" == "ubuntu" ]; then
     mkdir $HOME/scripts 2>/dev/null && mkdir $HOME/scripts/bash 2>/dev/null
     echo "#!/bin/bash" > $HOME/scripts/bash/streamtunnel.sh
     echo "(trap 'kill 0' SIGINT; ssh -vNL 4554:\$2:554 $atlas_username@\$1 & sleep 3 && /mnt/c/Program\ Files/VideoLAN/VLC/vlc.exe rtsp://localhost:4554)" >> $HOME/scripts/bash/streamtunnel.sh
